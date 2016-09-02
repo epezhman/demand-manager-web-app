ec1d4a23e94ad5684f14882eb062c5b63dfa8a64
Pezhman Nasirifard <epezhman@gmail.com>
2016-09-02 16:48:42 +0200

    with angular fire 2

--- client/app.module.ts
+++ client/app.module.ts
@@ -6,6 +6,8 @@ import {routing} from "./app.routes";
 import {BrowserModule} from "@angular/platform-browser";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {MeteorModule} from "angular2-meteor";
+import {LoginButtons} from 'angular2-meteor-accounts-ui'
+import {AngularFireModule} from 'angularfire2';
 import {AppComponent} from "./app.component";
 import {NavComponent} from "./components/navComponent/nav.component";
 import {WelcomeComponent} from "./components/welcomeComponent/welcome.component";
@@ -14,9 +16,18 @@ import {MapComponent} from "./components/mapComponent/map.component";
 
 enableProdMode();
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyAtWT98dejyLr9BQXkmxiTHbBtbKQ1ObnY",
+    authDomain: "tum-dm-fireb.firebaseapp.com",
+    databaseURL: "https://tum-dm-fireb.firebaseio.com",
+    storageBucket: "tum-dm-fireb.appspot.com",
+};
+
 @NgModule({
-    imports: [BrowserModule, routing, MeteorModule, FormsModule],
-    declarations: [AppComponent, NavComponent, WelcomeComponent, MapComponent],
+    imports: [BrowserModule, routing, MeteorModule, FormsModule,
+        AngularFireModule.initializeApp(firebaseConfig)],
+    declarations: [AppComponent, NavComponent, WelcomeComponent,
+        MapComponent, LoginButtons],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- client/components/mapComponent/map.component.ts
+++ client/components/mapComponent/map.component.ts
@@ -1,12 +1,19 @@
 import {Component} from "@angular/core";
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 //noinspection TypeScriptCheckImport
 import template from "./map.component.html";
 
+//noinspection TypeScriptUnresolvedVariable
 @Component({
+    moduleId: module.id,
     selector: 'map-section',
     template: template,
 })
 export class MapComponent {
+    items: FirebaseListObservable<any[]>;
 
+    constructor(af: AngularFire) {
+        this.items = af.database.list('devices');
+    }
 }
