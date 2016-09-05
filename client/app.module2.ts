1591cc71511a34fd15e5ba70b635107f26d054ff
Pezhman Nasirifard <epezhman@gmail.com>
2016-09-05 19:16:02 +0200

    working! agnularfire2

--- client/app.module2.ts
+++ client/app.module2.ts
@@ -1,36 +1,28 @@
 'use strict';
 
-import {NgModule, enableProdMode, provide} from "@angular/core";
+import {NgModule, enableProdMode} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {routing} from "./app.routes";
 import {BrowserModule} from "@angular/platform-browser";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {MeteorModule} from "angular2-meteor";
-import {LoginButtons} from 'angular2-meteor-accounts-ui'
-import {AngularFireModule} from 'angularfire2';
+import {AngularFireModule, WindowLocation} from "angularfire2";
 import {AppComponent} from "./app.component";
-import {NavComponent} from "./components/navComponent/nav.component";
-import {WelcomeComponent} from "./components/welcomeComponent/welcome.component";
-import {MapComponent} from "./components/mapComponent/map.component";
-import {WindowLocation} from "angularfire2";
-
 
 enableProdMode();
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyAtWT98dejyLr9BQXkmxiTHbBtbKQ1ObnY",
-    authDomain: "tum-dm-fireb.firebaseapp.com",
-    databaseURL: "https://tum-dm-fireb.firebaseio.com",
-    storageBucket: "tum-dm-fireb.appspot.com",
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    storageBucket: " ",
 };
-
 
 
 @NgModule({
     imports: [BrowserModule, routing, MeteorModule, FormsModule,
         AngularFireModule.initializeApp(firebaseConfig)],
-    declarations: [AppComponent, NavComponent, WelcomeComponent,
-        MapComponent, LoginButtons],
+    declarations: [AppComponent,],
     bootstrap: [AppComponent],
     providers: [{
         provide: WindowLocation, useValue: {
