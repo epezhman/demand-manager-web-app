29078ebc9772d26904a868845a0caaffa5e4ed5a
Pezhman Nasirifard <epezhman@gmail.com>
2016-10-02 16:21:33 +0200

    google maps integration

--- client/components/mapComponent/map.component.ts
+++ client/components/mapComponent/map.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from "@angular/core";
 import {AngularFire, FirebaseListObservable} from "angularfire2";
 import {Device} from "../../../lib/interfaces/device.interface";
+import * as _ from "lodash";
 //noinspection TypeScriptCheckImport
 import template from "./map.component.html";
-
 
 
 @Component({
@@ -13,23 +13,62 @@ import template from "./map.component.html";
 })
 export class MapComponent implements OnInit {
     devices: FirebaseListObservable<Device[]>;
+    devicesObservable: FirebaseListObservable<Device[]>;
+    devices: Device[] = [];
     isLoading: boolean = true;
-    lat_munich: number = 48.139;
-    lng_munich: number = 11.566;
-    zoom_munich: number = 11;
+    selectedDevices: string[] = [];
+
+    latMunich: number = 48.139;
+    lngMunich: number = 11.566;
+    zoomMunich: number = 11;
+
+    latCircle: number = 48.139;
+    lngCircle: number = 11.566;
+    radiusCircle: number = 1000;
+    colorCircle: string = "blue";
+    strokeColorCircle: string = "blue";
+
 
     constructor(private af: AngularFire) {
-
-
-    }
-
-    clickedMarker(label: string) {
-        console.log(`clicked the marker: ${label}`)
     }
 
     ngOnInit(): void {
-        this.devices = this.af.database.list('/online');
-        this.devices.subscribe(() => this.isLoading = false);
+        this.devicesObservable = this.af.database.list('/online');
+        this.devicesObservable.subscribe((devicesData)=> {
+            this.devices = devicesData;
+            this.isLoading = false
+        });
+    }
+
+    clickedMarker(deviceId: string) {
+        if (this.selectedDevices.indexOf(deviceId) < 0) {
+            this.selectedDevices.push(deviceId);
+        }
+        else {
+            _.pull(this.selectedDevices, deviceId);
+        }
+    }
+
+    radiusChange(radius: number) {
+        this.radiusCircle = radius;
+    }
+
+    centerChange(center) {
+        this.latCircle = center['lat'];
+        this.lngCircle = center['lng'];
+    }
+
+    onlineStatus(device: Device): string {
+        if (!device)
+            return '';
+        if (device.connections)
+            return '/images/green-dot.png';
+        return '/images/red-dot.png';
+
+    }
+
+    loadingDeviceDetail(loading: boolean): void {
+        this.isLoading = loading;
     }
 
 
